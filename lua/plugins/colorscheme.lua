3461a0df5daa3e59eda7d33bb54d83941dadb39a
Will Cameron <will@wills-mbp.lan>
2023-09-25 19:33:36 -0700

    update colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -5,21 +5,12 @@ return {
   { "catppuccin/nvim", name = "catppuccin" },
   {
     "projekt0n/caret.nvim",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      require("caret").setup({
-        -- ...
-      })
-
-      vim.cmd("colorscheme caret")
-    end,
   },
   -- Configure LazyVim to load theme
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "caret",
+      colorscheme = "kanagawa",
     },
   },
 }
