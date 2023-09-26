5063c0ce02c507e7a551738c1f580d1f8c50eef0
Will Cameron <cameron.will@gmail.com>
2023-09-25 17:51:45 -0700

    simplify config

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,32 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- Having longer updatetime (default is 4000 ms = 4s) leads to noticeable
+-- delays and poor user experience
+vim.opt.updatetime = 300
+
+-- Always show the signcolumn, otherwise it would shift the text each time
+-- diagnostics appeared/became resolved
+vim.opt.signcolumn = "yes"
+
+local mark = require("harpoon.mark")
+local ui = require("harpoon.ui")
+
+-- Harpoon Keymaps
+vim.keymap.set("n", "<leader>a", mark.add_file, { desc = "Add file to harpoon" })
+vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+
+-- UFO Keymaps
+vim.o.foldcolumn = "1" -- '0' is not bad
+vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
+
+-- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
+vim.keymap.set("n", "zR", require("ufo").openAllFolds)
+vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
